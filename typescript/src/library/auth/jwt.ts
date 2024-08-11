2bb1e98dd0526cc00dc1925001d7c047bf3a8f3b
BuildTools <unconfigured@null.spigotmc.org>
2024-08-11 15:13:06 +0200

    chore: Update landing page banner color, add lazy loading for images, and improve file view layout

--- typescript/src/library/auth/jwt.ts
+++ typescript/src/library/auth/jwt.ts
@@ -5,6 +5,13 @@ function base64url_encode(buffer) {
         .replace(/=+$/, '');
 }
 
+function base64URLdecode(str) {
+    const base64Encoded = str.replace(/-/g, '+').replace(/_/g, '/');
+    const padding = str.length % 4 === 0 ? '' : '='.repeat(4 - (str.length % 4));
+    const base64WithPadding = base64Encoded + padding;
+    return atob(base64WithPadding);
+  }
+
 export const JWT = {
     settings: {
         secret: Bun.env.JWT_SECRET || "secret",
@@ -32,15 +39,24 @@ export const JWT = {
     payloadFromToken: (token) => {
         let payload = token.split(".")[1];
         return JSON.parse(base64URLdecode(payload));
+    },
+    verifyJWT: async (req: Request): Promise<boolean> => {
+        const cookies = {};
+        req.headers.get("cookie")?.split(";").forEach((cookie: string) => {
+            let [key, value] = cookie.split("=");
+            cookies[key.trim()] = value;
+        });
+
+        if(cookies["token"] == undefined) return false;
+
+        return JWT.verify(cookies["token"]);
+    },
+    middleware: (redirectPath: string) => async (req, next) => {
+        let response = await JWT.verifyJWT(req);
+        if(response == false) return Response.redirect(redirectPath);
+        return next();
     }
 }
-
-function base64URLdecode(str) {
-    const base64Encoded = str.replace(/-/g, '+').replace(/_/g, '/');
-    const padding = str.length % 4 === 0 ? '' : '='.repeat(4 - (str.length % 4));
-    const base64WithPadding = base64Encoded + padding;
-    return atob(base64WithPadding);
-  }
 
 export function SHA256_to_HEX(data) {
     const hasher = new Bun.CryptoHasher("sha256");
